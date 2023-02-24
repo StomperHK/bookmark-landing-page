3a429501f068e62a0744d43e487e86767b9a7ad3
Rafael Ribeiro <cortadordeunha666@gmail.com>
2023-02-24 18:24:34 -0300

    feat: add intecractions.

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ const tabsVerticalBarEL = document.querySelector('[data-js="features-section__ta
 const featuresCarouselEL = document.querySelector('[data-js="features-section__carousel"]')
 let tabIndexGlobal = 0
 
+const accordionContainerEL = document.querySelector('[data-js="questions-section__accordion-container"]')
+const accordionButtonsELs = Array.from(accordionContainerEL.querySelectorAll('button'))
+const accordionParagraphsELs = Array.from(accordionContainerEL.querySelectorAll('p'))
+
 
 function highlightSelectedTab() {
-  tabsELs.forEach(tabEL => tabEL.classList.remove('features-section__tabs__tab--active'))
+  const highlightedTab = tabsELs[tabIndexGlobal]
 
-  tabsELs[tabIndexGlobal].classList.add('features-section__tabs__tab--active')
+  tabsELs.forEach(tabEL => {
+    tabEL.classList.remove('features-section__tabs__tab--active')
+    tabEL.ariaSelected = "false"
+  })
+
+  highlightedTab.classList.add('features-section__tabs__tab--active')
+  highlightedTab.ariaSelected = "true"
 }
 
 function changeCarouselTab(tabIndex) {
@@ -26,9 +36,31 @@ function changeCarouselTab(tabIndex) {
   highlightSelectedTab()
 }
 
+function toggleAccordion(accordionButonEL, accordionIndex) {
+  const accordionParagraph = accordionParagraphsELs[accordionIndex]
+  const accordionArrowEL = accordionButonEL.querySelector('svg')
 
+  if (accordionParagraph.dataset.expanded === "true") {
+    accordionParagraph.style.height = "0px"
+    accordionParagraph.dataset.expanded = "false"
+    accordionParagraph.ariaExpanded = "false"
+
+    accordionArrowEL.classList.remove('rotate-arrow')
+  }
+  else {
+    const paragraphHeight = accordionParagraph.scrollHeight + 'px'
+    
+    accordionParagraph.style.height = paragraphHeight
+    accordionParagraph.dataset.expanded = "true"
+    accordionParagraph.ariaExpanded = "true"
+
+    accordionArrowEL.classList.add('rotate-arrow')
+  }
+}
 
 
 tabsELs.forEach((tabEL, tabIndex) => tabEL.addEventListener('click', () => changeCarouselTab(tabIndex)))
+
+accordionButtonsELs.forEach((accordionButonEL, accordionIndex) => accordionButonEL.addEventListener('click', () => toggleAccordion(accordionButonEL, accordionIndex)))
 
 window.addEventListener('resize', () => changeCarouselTab(false))
